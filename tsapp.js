05bf485da18bcaf9f55e45553d56f986877c25bd
Kerberus Ingeniería <>
2016-07-06 11:30:21 -0500

    changing from app.js to [tsapp.ts -> tsapp.js]

---
+++ tsapp.js
@@ -0,0 +1,20 @@
+"use strict";
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var Server = (function () {
+    function Server() {
+        this.app = express();
+        this.app.use(logger('dev'));
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(cookieParser());
+        this.app.use(express.static(path.join(__dirname, 'public')));
+    }
+    return Server;
+}());
+var appObj = new Server();
+module.exports = appObj.app;
+//# sourceMappingURL=tsapp.js.map
---
+++ tsapp.ts
@@ -0,0 +1,23 @@
+/// <reference path="typings/index.d.ts" />
+import * as express from 'express';
+import * as path from 'path';
+import * as logger from 'morgan';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
+
+class Server
+{
+    app: express.Application;
+    constructor()
+    {
+        this.app = express();
+        this.app.use(logger('dev'))
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({extended: false}));
+        this.app.use(cookieParser());
+        this.app.use(express.static(path.join(__dirname, 'public')));
+    }
+}
+
+let appObj = new Server();
+export = appObj.app;
