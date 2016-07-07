b07c70cb808ddeb460bb85574ae61c354c818f51
Kerberus Ingeniería <>
2016-07-07 17:47:01 -0500

    Class nClass extends Help

--- public/bundle.js
+++ public/bundle.js
@@ -31583,6 +31583,11 @@
 /***/ function(module, exports) {
 
 	"use strict";
+	var __extends = (this && this.__extends) || function (d, b) {
+	    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+	    function __() { this.constructor = d; }
+	    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+	};
 	var controller;
 	(function (controller) {
 	    var Main = (function () {
@@ -31595,11 +31600,25 @@
 	    controller.Main = Main;
 	    var Help = (function () {
 	        function Help() {
-	            console.log('Controlador HELP');
+	            this.persona = {};
+	            this.persona.nombre = "Help";
+	            console.log('Controlador HELP ');
 	        }
+	        Help.prototype.doSomething = function (texto) {
+	            console.log(texto);
+	        };
 	        return Help;
 	    }());
 	    controller.Help = Help;
+	    var nClass = (function (_super) {
+	        __extends(nClass, _super);
+	        function nClass() {
+	            _super.call(this);
+	            this.persona.apellido = "nClass";
+	            this.doSomething(this.persona.apellido + " " + this.persona.apellido + " está haciendo algo.");
+	        }
+	        return nClass;
+	    }(Help));
 	})(controller = exports.controller || (exports.controller = {}));
 
 
